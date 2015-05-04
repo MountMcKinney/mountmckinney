8842b98578f97f4f51f5580e7ce36251eab54f0d
MountMcKiki <mountmckiki@gmail.com>
2015-05-04 11:33:07 -0400

    Initial Build On Mac

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var $title = $('.title');
+
+$title.hide().fadeIn(3500);
+
+//(function() {
+//
+//  "use strict";
+//
+//  var toggles = document.querySelectorAll(".cmn-toggle-switch");
+//
+//  for (var i = toggles.length - 1; i >= 0; i--) {
+//    var toggle = toggles[i];
+//    toggleHandler(toggle);
+//  };
+//
+//  function toggleHandler(toggle) {
+//    toggle.addEventListener( "click", function(e) {
+//      e.preventDefault();
+//      (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
+//    });
+//  }
+//})();
+//
+//(function(){
+//   $( '.menu-btn' ).onclick(function(){
+//      $('.responsive-menu').toggleClass('expand')
+//   })
+//});
