fb9c34b7f355a1f55500066be01f135550f50f6f
MountMcKiki <mountmckiki@gmail.com>
2015-05-04 17:05:02 -0400

    Forms Added To Page
    Google forms added as placeholders for fully customized forms

--- js/app.js
+++ js/app.js
@@ -1,28 +1,41 @@
-var $title = $('.title');
-
-$title.hide().fadeIn(3500);
-
-//(function() {
+// var $title = $('.title');
+// var $menu-btn = $('.menu-btn');
+// var $menu-btn-index = $('.menu-btn-index');
+// var $responsive-menu = $('.responsive-menu');
 //
-//  "use strict";
+// $title.hide().fadeIn(3500);
 //
-//  var toggles = document.querySelectorAll(".cmn-toggle-switch");
-//
-//  for (var i = toggles.length - 1; i >= 0; i--) {
-//    var toggle = toggles[i];
-//    toggleHandler(toggle);
-//  };
-//
-//  function toggleHandler(toggle) {
-//    toggle.addEventListener( "click", function(e) {
-//      e.preventDefault();
-//      (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
-//    });
-//  }
-//})();
-//
-//(function(){
-//   $( '.menu-btn' ).onclick(function(){
-//      $('.responsive-menu').toggleClass('expand')
-//   })
-//});
+// function(){
+//    $menu-btn.click(function(){
+//      $responsive-menu.toggleClass('expand');
+//    })
+// };
+// function(){
+//    $menu-btn-index.click(function(){
+//      $.responsive-menu.toggleClass('expand');
+//    })
+// };
+// //(function() {
+// //
+// //  "use strict";
+// //
+// //  var toggles = document.querySelectorAll(".cmn-toggle-switch");
+// //
+// //  for (var i = toggles.length - 1; i >= 0; i--) {
+// //    var toggle = toggles[i];
+// //    toggleHandler(toggle);
+// //  };
+// //
+// //  function toggleHandler(toggle) {
+// //    toggle.addEventListener( "click", function(e) {
+// //      e.preventDefault();
+// //      (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
+// //    });
+// //  }
+// //})();
+// //
+// //(function(){
+// //   $( '.menu-btn' ).onclick(function(){
+// //      $('.responsive-menu').toggleClass('expand')
+// //   })
+// //});
