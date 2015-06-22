641febbe53d9dd111b14a5fc76ec67603cd61904
mountmckinney <john@mountmckinney.com>
2015-06-22 08:26:15 -0400

    Started the move to Twig Templates
    Need to create Twig extensions to use PHP in the Template. Search Use
    PHP in Twig.

--- index.php
+++ index.php
@@ -1,23 +1,137 @@
 <?php
-require_once("inc/config.php");
-?>
-<!DOCTYPE html>
-<html id="homepage">
-<?php
-  $pageTitle="Home";
-  include(ROOT_PATH."inc/header.php");
-?>
-    <header>
-      <?php include(ROOT_PATH."inc/name_title_header.php");?>
-      </header>
-    <nav>
-      <?php include(ROOT_PATH."inc/menu.php");?>
-    </nav>
-    <div class="home-header">
-        <h1 class='title'>Mount McKinney</h1>
-        <p class='title'>Adventure is calling</p>
-      </div>
-    <div id='home-footer'>
-      <?php include(ROOT_PATH."inc/footer.php"); ?>
-    </div>
-      <?php include(ROOT_PATH."inc/js_scripts.php");?>
+      require 'vendor/autoload.php';
+      date_default_timezone_set('America/New_York');
+
+      //$log = new Monolog\Logger('name');
+      //$log->pushHandler(new Monolog\Handler\StreamHandler('app.txt', Monolog\Logger::WARNING));
+      //$log->addWarning('Oh Noes.');
+
+      $app = new \Slim\Slim( array(
+        'view' => new \Slim\Views\Twig()
+      ));
+
+      $view = $app->view();
+      $view->parserOptions = array(
+          'debug' => true
+      );
+      $view->parserExtensions = array(
+          new \Slim\Views\TwigExtension(),
+      );
+
+      $app->get('/', function() use($app){
+        $app->render('main.twig');
+      })->name('home');
+
+      $app->get('/contact', function() use($app){
+        $app->render('contact.twig');
+      })->name('contact');
+
+      $app->get('/portfolio', function() use($app){
+        $app->render('portfolio.twig');
+      })->name('portfolio');
+
+      $app->get('/example', function() use($app){
+        $app->render('example.twig');
+      })->name('example');
+
+      $app->get('/subscribe', function() use($app){
+        $app->render('subscribe.twig');
+      })->name('subscribe');
+
+      $app->get('/twocents', function() use($app){
+        $app->render('twocents.twig');
+      })->name('twocents');
+
+      $app->get('/tcblog', function() use($app){
+        $app->render('tcblog.twig');
+      })->name('tcblog');
+
+      $app->get('/campfirestories', function() use($app){
+        $app->render('campfirestories.twig');
+      })->name('campfirestories');
+
+      $app->get('/campfire', function() use($app){
+        $app->render('campfire.twig');
+      })->name('campfire');
+
+      $app->post('/portfolio', function() use($app){
+        $name = $app->request->post('name');
+        $email = $app->request->post('email');
+        $msg = $app->request->post('msg');
+
+        if(!empty($name) && !empty($email) && !empty($msg)) {
+          $cleanName = filter_var($name, FILTER_SANITIZE_STRING);
+          $cleanEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
+          $cleanMsg = filter_var($msg, FILTER_SANITIZE_STRING);
+        } else {
+          //message the user there was a problem
+          $app->redirect('/contact');
+        }
+
+        $transport = Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
+        $mailer = \Swift_Mailer::newInstance($transport);
+        $message = \Swift_Message::newInstance();
+        $message->setSubject('Email From Our Website');
+
+        //setFrom and setTo will be flipped in the way I will be using the mailer
+        $message->setFrom(array(
+          'john@mountmckinney.com' => 'John McKinney'
+        ));
+        $message->setTo(array($cleanEmail => $cleanName));
+        //This would be a message I created thanking them for subscribing, and doing a call to action in the email to our social pages and back to the website
+        $message->setBody($contactThankYou);
+
+
+        $result = $mailer->send($message);
+
+        if($result > 0) {
+          //send Thank you message to page
+          $app->redirect('/');
+        } else {
+          //send message to user that message failed to send
+          //log error
+          $app->redirect('/portfolio');
+        }
+      });
+
+      $app->post('/contact', function() use($app){
+        $name = $app->request->post('name');
+        $email = $app->request->post('email');
+        $msg = $app->request->post('msg');
+
+        if(!empty($name) && !empty($email) && !empty($msg)) {
+          $cleanName = filter_var($name, FILTER_SANITIZE_STRING);
+          $cleanEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
+          $cleanMsg = filter_var($msg, FILTER_SANITIZE_STRING);
+        } else {
+          //message the user there was a problem
+          $app->redirect('/contact');
+        }
+
+        $transport = Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
+        $mailer = \Swift_Mailer::newInstance($transport);
+        $message = \Swift_Message::newInstance();
+        $message->setSubject('Email From Our Website');
+
+        //setFrom and setTo will be flipped in the way I will be using the mailer
+        $message->setFrom(array(
+          'john@mountmckinney.com' => 'John McKinney'
+        ));
+        $message->setTo(array($cleanEmail => $cleanName));
+        //This would be a message I created thanking them for subscribing, and doing a call to action in the email to our social pages and back to the website
+        $message->setBody($contactThankYou);
+
+
+        $result = $mailer->send($message);
+
+        if($result > 0) {
+          //send Thank you message to page
+          $app->redirect('/');
+        } else {
+          //send message to user that message failed to send
+          //log error
+          $app->redirect('/contact');
+        }
+      });
+
+      $app->run();
