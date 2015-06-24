4cea94d68905dde48ac8bce392c815b125d3b84c
John <jmckinney448@gmail.com>
2015-06-24 18:56:31 -0400

    Small changes

--- inc/subscribe-form.php
+++ inc/subscribe-form.php
@@ -3,19 +3,32 @@
 <div id="sub">
   <!-- // if status=thanks in the query string, display an thank you message instead of the form -->
 <?php if (isset($_GET["status"]) AND $_GET["status"] == "thanks") { ?>
-<?php include(ROOT_PATH.'inc/contact-thankyou.php'); ?>
+  <?php include(ROOT_PATH.'inc/subscribe-thankyou.php'); ?>
 <?php } else { ?>
-<?php
-if (isset($error_message)) {
-echo '<p class="message">' . $error_message . '</p>';
-}
-?>
-    <form class="sub-form" id="sub-formpage">
+  <?php
+    if (isset($error_message)) {
+    echo '<p class="message">' . $error_message . '</p>';
+    }
+  ?>
+    <form class="sub-form" id="sub-formpage" method="post">
             <label for="name">Name</label>
-            <input type="text" id="sub_form_name" name="sub_name" class="required"/>
+              <input type="text" id="sub_form_name" name="sub_name" value="<?php if (isset($name)) { echo htmlspecialchars($name); } ?>">
             <label for="email">Email</label>
-            <input type="email" id="sub_form_email" name="sub_email" class="required"/>
-            <button id="ButtonSubmit" class="submit-button-form" type="submit">Connect</button>
+              <input type="email" id="sub_form_email" name="sub_email" value="<?php if(isset($email)) { echo htmlspecialchars($email); } ?>">
+              <table style="display: none;">
+                <tr>
+                  <!-- // the field named address is used as a spam honeypot-->
+                  <!-- // it is hidden from users, and it must be left blank-->
+                  <th>
+                    <label for="address">Address</label>
+                  </th>
+                    <td>
+                      <input type="text" name="address" id="address">
+                        <p>Humans: please leave this field blank.</p>
+                    </td>
+                </tr>
+              </table>
+            <button id="subButtonSubmit" class="submit-button-form" type="submit">Connect</button>
         </form>
       </div>
         <?php } ?>
