6fb2f25ff26131570c68d7afcb9926aedd531e39
John <mountmckiki@gmail.com>
2016-01-12 14:27:44 -0500

    Changed Menu To Reflect Changes

--- inc/menu.php
+++ inc/menu.php
@@ -12,7 +12,7 @@
       <a href="<?php echo BASE_URL; ?>portfolio/"><li>Portfolio</li></a>
       <a href="<?php echo BASE_URL; ?>services/"><li>Services</li></a>
       <a href="<?php echo BASE_URL; ?>campfirestories/"><li>Campfire Stories</li></a>
-      <a href="<?php echo BASE_URL; ?>twocents/"><li>My Two Cents</li></a>
+      <!-- <a href="<?php echo BASE_URL; ?>twocents/"><li>My Two Cents</li></a> -->
       <a href="<?php echo BASE_URL; ?>contact/"><li>Contact</li></a>
       <a href="<?php echo BASE_URL; ?>subscribe/"><li>Subscribe</li></a>
       </ul>
@@ -23,12 +23,12 @@
       <a href="<?php echo BASE_URL; ?>"><li>Home</li></a>
       <a href="<?php echo BASE_URL; ?>portfolio/"><li>Portfolio</li></a>
       <a href="<?php echo BASE_URL; ?>services/"><li>Services</li></a>
-      <li class="blog">Blog
-        <ul>
+      <!-- <li class="blog">Blog
+        <ul> -->
           <a href="<?php echo BASE_URL; ?>campfirestories/"><li>Campfire Stories</li></a>
-          <a href="<?php echo BASE_URL; ?>twocents/"><li>My Two Cents</li></a>
-        </ul>
-      </li>
+          <!-- <a href="<?php echo BASE_URL; ?>twocents/"><li>My Two Cents</li></a> -->
+        <!-- </ul>
+      </li> -->
       <a href="<?php echo BASE_URL; ?>contact/"><li>Contact</li></a>
       <a href="<?php echo BASE_URL; ?>subscribe/"><li>Subscribe</li></a>
   </ul>
