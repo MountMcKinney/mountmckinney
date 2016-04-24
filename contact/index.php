a216e379567604f98d8ee7da07be57909ff33757
John <mountmckiki@gmail.com>
2016-04-24 00:44:30 -0400

    Need to overhaul some styling decisions

--- contact/index.php
+++ contact/index.php
@@ -14,9 +14,9 @@ require_once("../inc/config.php");
 <div id="wrapper-contact">
   <div class="contact-details">
     <ul class="details">
-      <li class="mail"><a href="mailto:john@mountmckinney.com"><img src="<?php echo BASE_URL; ?>img/mail.png"></a></li>
-      <li class="twitter"><a href="http://twitter.com/intent/tweet?screen_name=mountmckinney" target="_blank" "social" id="twitter-icon"><img src="<?php echo BASE_URL; ?>img/twitter-icon.svg"></a></li>
-      <li class="facebook"><a href="http://facebook.com/mountmckinney" target="_blank" class="social" id="facebook-icon"><img src="<?php echo BASE_URL; ?>img/facebook-icon.svg"></a></li>
+      <li class="mail"><a href="mailto:john@mountmckinney.com"><img src="<?php echo BASE_URL; ?>img/social/mail.png"></a></li>
+      <li class="twitter"><a href="http://twitter.com/intent/tweet?screen_name=mountmckinney" target="_blank" "social" id="twitter-icon"><img src="<?php echo BASE_URL; ?>img/social/twitter-icon.svg"></a></li>
+      <li class="facebook"><a href="http://facebook.com/mountmckinney" target="_blank" class="social" id="facebook-icon"><img src="<?php echo BASE_URL; ?>img/social/facebook-icon.svg"></a></li>
     </ul>
       <?php include(ROOT_PATH.'inc/contactButton.php'); ?>
       <?php include(ROOT_PATH.'inc/contactFormPopup.php'); ?>
